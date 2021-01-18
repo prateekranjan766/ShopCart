cc6937cfdafbcbacf91777c8e55b674f956697f1
Prateek Ranjan <prateekranjan766@gmail.com>
2021-01-18 13:08:14 +0530

    basic product routes created and express-async-handler installed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './data/products.js';
 import connectDB from './config/db.js';
 import colors from 'colors';
+import productRoutes from './routes/productRoutes.js';
 
 //loading .env file in the backend to process.env
 dotenv.config();
@@ -17,13 +17,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(`Api running...`);
 });
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-app.get(`/api/products/:id`, (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+
+app.use('/api/products', productRoutes);
 
 //Server Starting
 const PORT = process.env.PORT || 5000;
